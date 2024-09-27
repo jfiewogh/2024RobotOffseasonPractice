76f77159cde9d6b947b3d7e1e425573cb28fd7cc
David Chau <86276480+jfiewogh@users.noreply.github.com>
2024-09-27 12:29:13 -0500

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -28,31 +28,18 @@ public final class Constants {
   }
 
   public static class AbsoluteEncoderConstants {
-    // to move the wheel clockwise, increase
-    // to move the wheel counterclockwise, decrease
-
-
+    // positive is clockwise, negative is counterclockwise
     /*
-    FL: 10.67766447874932 0.498291015625 10.677664620535714
-FR: 10.756139405981244 0.501708984375 10.750906808035714
-BL: 10.77183366287412 0.50244140625 10.7666015625
-BR: 10.635811508027185 0.49658203125 10.641043526785714
-
-FL: 4.606176971637067 0.21826171875 4.677036830357142
-FR: 18.065561361790845 0.843505859375 18.07512555803571
-BL: 14.343222404184024 0.67138671875 14.386858258928571
-BR: 20.326151196337705 0.947998046875 20.314243861607142
-
-    
-     * 
+     * HOW TO GET THE VALUES
+     * set the offsets to 0
+     * run the robot on arcade drive, and align the wheels so that they are all facing forward
+     * record the absolute encoder values
+     * set the offsets to the negative of the recorded values
+     * (To see the offsets, you have to run twice for some reason.)
      */
-    
-     // to see offset, you have to run twice, for some reason
-
-    // set offset to 0, align the wheels then record absolute encoder values
-    public static final double kFrontLeftOffset = -(0.21826171875 - 0.498291015625 + 0.5); // 0.84743377924 - 0.1;
-    public static final double kFrontRightOffset = -(0.843505859375 - 0.501708984375 + 0.5); // 0.20410059132;
-    public static final double kBackLeftOffset = -(0.67138671875 - 0.50244140625 + 0.5); // 0.29394180287;
-    public static final double kBackRightOffset = -(0.947998046875 - 0.49658203125 + 0.5); // 0.07128637592;
+    public static final double kFrontLeftOffset = -(0.21826171875);
+    public static final double kFrontRightOffset = -(0.843505859375);
+    public static final double kBackLeftOffset = -(0.67138671875);
+    public static final double kBackRightOffset = -(0.947998046875);
   }
 }
